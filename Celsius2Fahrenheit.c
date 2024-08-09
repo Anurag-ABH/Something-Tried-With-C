9150df75eb162a15ceed15b21f756870745aaeda
Anurag Das <dasanurag0606@gmail.com>
2024-08-10 00:59:21 +0530

    Update with °F 2 °C

--- Celsius2Fahrenheit.c
+++ Celsius2Fahrenheit.c
@@ -8,4 +8,12 @@ int main() {
     f = (c * 1.8) + 32;
     printf("\nTempareture in Fahrenheit(°C): %f", f);
     // °F as Output
+
+    float d, e;
+    printf("\nEnter Fahrenheit(°F): ");
+    scanf("%f", &d);
+    // °F as Output
+    e = (d - 32) / 1.8;
+    printf("\nTempareture in Celsius(°C): %f", e);
+    // Taking Celsius
 }
